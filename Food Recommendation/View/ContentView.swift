2f218f60635f441c13d34d872733f03abf823958
Jean Carlos Quejada Toro <jean.quejada@sioma.app>
2022-02-18 08:36:55 -0500

    REFACTOR: Remuevo el scroll bar de la lista

--- Food Recommendation/View/ContentView.swift
+++ Food Recommendation/View/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView{
+            ScrollView(showsIndicators:false){
                 ForEach(filterRating,id:\.id) { food in
                     FoodCardView(
                         foodItem: food,
