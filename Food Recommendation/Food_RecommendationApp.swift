e583ea9d3ccb7faf9ee680eeb9f9c1da45a06bf7
Jean Carlos Quejada Toro <jean.quejada@sioma.app>
2022-02-17 22:04:47 -0500

    Initial Commit

---
+++ Food Recommendation/Food_RecommendationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_RecommendationApp.swift
+//  Food Recommendation
+//
+//  Created by Sioma on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Food_RecommendationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
