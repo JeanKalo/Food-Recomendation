1aec1c1b6f74f19e721bb7271ce621c4586529ec
Jean Carlos Quejada Toro <jean.quejada@sioma.app>
2022-02-18 08:34:38 -0500

    NEW FEAT:  menos radios para la sombra de la tarjeta y animación de blink heart más transición de cambio

--- Food Recommendation/View/FoodCardView.swift
+++ Food Recommendation/View/FoodCardView.swift
@@ -13,61 +13,77 @@ struct FoodCardView : View {
     
     @Binding var isUserFavorite : Bool
     
+    @State var animateHeart : Bool = false
+    
     var body: some View{
-        HStack {
-            VStack(alignment: .leading, spacing: 2) {
-                //MARK: nombre
-                Text(foodItem.name.capitalized)
-                    .lineLimit(2)
-                    .font(.system(size: 18))
-                //MARK: descripción
-                Text(foodItem.description)
-                    .lineLimit(2)
-                    .font(.system(size: 12))
-                    .foregroundColor(.black.opacity(0.9))
-                Spacer()
-                HStack {
-                    //MARK: likes
-                    Text(foodItem.numberOfLikes.description)
-                        .lineLimit(1)
-                        .font(.system(size: 12,weight: .bold))
-                        .foregroundColor(.pink)
-                    //MARK: boton de me gusta
-                    Image(systemName:isUserFavorite ? "heart.fill":  "heart")
-                        .foregroundColor(.pink)
-                        .scaleEffect(isUserFavorite ? 1.5 : 1.0)
-                        .opacity(isUserFavorite ? 1.0 : 0.5)
+        VStack {
+            HStack {
+                VStack(alignment: .leading, spacing: 2) {
+                    //MARK: nombre
+                    Text(foodItem.name.capitalized)
+                        .lineLimit(2)
+                        .font(.system(size: 18))
+                    //MARK: descripción
+                    Text(foodItem.description)
+                        .lineLimit(2)
+                        .font(.system(size: 12))
+                        .foregroundColor(.black.opacity(0.9))
+                    Spacer()
+                    HStack {
+                        //MARK: likes
+                        Text(foodItem.numberOfLikes.description)
+                            .lineLimit(1)
+                            .font(.system(size: 12,weight: .bold))
+                            .foregroundColor(.pink)
+                        //MARK: boton de me gusta
+                        Image(systemName:animateHeart ? "heart.fill":  "heart")
+                            .foregroundColor(.pink)
+                            .scaleEffect(animateHeart ? 1.5 : 1.0)
+                            .opacity(animateHeart ? 1.0 : 0.5)
+                    }
                 }
+               
+                Spacer(minLength: 150)
             }
-           
-            Spacer(minLength: 150)
-        }
-        .padding()
-        .frame(width: FoodModelData.GetScreenSize() - 40, height: 110)
-        .background(foodItem.cardColor)
-        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-        .shadow(color: .gray.opacity(0.5), radius: 7, x: 2, y: 8)
-        .onTapGesture(count: 2){
-            if foodItem.isUserFavorite {
-                withAnimation(Animation.default) {
-                    isUserFavorite = false
+            .padding()
+            .frame(width: FoodModelData.GetScreenSize() - 40, height: 110)
+            .background(foodItem.cardColor)
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            .shadow(color: .gray.opacity(0.5), radius: 5, x: 2, y: 6)
+            .onTapGesture(count: 2){
+                if animateHeart {
+                    withAnimation(Animation.default.repeatCount(1)) {
+                        animateHeart = false
+                    }
+                }else{
+                    withAnimation(Animation.default.repeatCount(3)) {
+                        animateHeart = true
+                    }
                 }
-            }else{
-                withAnimation(Animation.default) {
-                    isUserFavorite = true
+               
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    //MARK: despues de 2 segundos le pedimos a los datos que cambien su estado
+                    withAnimation(Animation.easeInOut){
+                        isUserFavorite.toggle()
+                    }
+                   
                 }
+                
+               
             }
-        }
-        .overlay(
-            //MARK: imagen
-            foodItem.image
-                .resizable()
-                .aspectRatio( contentMode: .fill)
-                .frame(width: 120, height: 150)
-                .padding(0)
-            ,alignment: .bottomTrailing
+            .overlay(
+                //MARK: imagen
+                foodItem.image
+                    .resizable()
+                    .aspectRatio( contentMode: .fill)
+                    .frame(width: 120, height: 150)
+                    .padding(0)
+                ,alignment: .bottomTrailing
 
         )
+        }
+        .padding(.bottom,20)
     }
     
     
